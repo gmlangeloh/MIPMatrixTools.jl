c990492c589ea86d05990b7f844e57ef1e4122d9
Gabriel Mattos Langeloh <gmlangeloh@inf.ufrgs.br>
2023-10-16 14:35:21 -0300

    Better experiments for LAP, cover, packing

--- test/various_problems.jl
+++ test/various_problems.jl
@@ -20,19 +20,30 @@ function repeated_subsets(subsets :: Vector{Set{Int}})
 end
 
 function is_feasible_set_cover(subsets :: Vector{Set{Int}}, n :: Int)
+    if isempty(subsets)
+        return n == 0
+    end
     return foldl(union, subsets) == Set(1:n)
 end
 
-function generate_subsets(n :: Int, m :: Int, p :: Float64)
+is_feasible_set_packing(s :: Vector{Set{Int}}, :: Int) = length(s) > 0
+
+function generate_subsets(n :: Int, m :: Int, p :: Float64, feasibility_check)
     subsets = Vector{Set{Int}}()
-    for i in 1:m
-        subset = Set{Int}()
-        for j in 1:n
-            if rand() < p
-                push!(subset, j)
+    while !feasibility_check(subsets, n) || repeated_subsets(subsets)
+        empty!(subsets)
+        while length(subsets) < m
+            subset = Set{Int}()
+            for j in 1:n
+                if rand() < p
+                    push!(subset, j)
+                end
             end
+            if isempty(subset)
+                continue
+            end
+            push!(subsets, subset)
         end
-        push!(subsets, subset)
     end
     return subsets
 end
@@ -43,7 +54,7 @@ end
 #one of the chosen subsets.
 #p is the probability any given element is in a subset.
 function generate_set_cover(n :: Int, m :: Int, p :: Float64)
-    subsets = generate_subsets(n, m, p)
+    subsets = generate_subsets(n, m, p, is_feasible_set_cover)
     model = Model()
     @variable(model, x[1:m], Bin)
     @objective(model, Min, sum(x[i] for i in 1:m))
@@ -58,7 +69,7 @@ end
 #number of subsets such that no element is contained in more than one of them.
 #p is the probability any given element is in a subset.
 function generate_set_packing(n :: Int, m :: Int, p :: Float64)
-    subsets = generate_subsets(n, m, p)
+    subsets = generate_subsets(n, m, p, is_feasible_set_packing)
     model = Model()
     @variable(model, x[1:m], Bin)
     @objective(model, Max, sum(x[i] for i in 1:m))
@@ -71,12 +82,58 @@ end
 using MIPMatrixTools.IPInstances
 using IPGBs.FourTi2
 using IPGBs
-function test_lap()
-    lap_model, _ = generate_lap(5)
-    lap = IPInstance(lap_model, infer_binary=false)
-    gb = groebner(lap)
-    @show size(gb)
-    gb2 = groebner_basis(lap)
-    @show length(gb2)
-    lap
+function test_lap(n, reps = 1)
+    for rep in 1:reps
+        lap_model, _ = generate_lap(n)
+        lap = IPInstance(lap_model, infer_binary=false)
+        gb, t4ti2, _, _, _ = @timed groebner(lap)
+        println("LAP & 4ti2 & ", n, " & 0 & 0 & ", rep, " & ", size(gb, 2), " & ", size(gb, 1), " & ", t4ti2)
+        gb2, tipgbs, _, _, _ = @timed groebner_basis(lap)
+        println("LAP & IPGBs & ", n, " & 0 & 0 & ", rep, " & ", size(gb, 2), " & ", length(gb2), " & ", tipgbs)
+    end
+end
+
+function test_set_cover(n, m, p, reps = 1)
+    for rep in 1:reps
+        cov_model, _ = generate_set_cover(n, m, p)
+        cov = IPInstance(cov_model, infer_binary=true)
+        gb, t4ti2, _, _, _ = @timed groebner(cov)
+        println("Cover & 4ti2 & ", n, " & ", m, " & ", p, " & ", rep, " & ", size(gb, 2), " & ", size(gb, 1), " & ", t4ti2)
+        gb2, tipgbs, _, _, _ = @timed groebner_basis(cov)
+        println("Cover & IPGBs & ", n, " & ", m, " & ", p, " & ", rep, " & ", size(gb, 2), " & ", length(gb2), " & ", tipgbs)
+    end
+end
+
+function test_set_packing(n, m, p, reps = 1)
+    for rep in 1:reps
+        pack_model = generate_set_packing(n, m, p)
+        pack = IPInstance(pack_model, infer_binary=true)
+        gb, t4ti2, _, _, _ = @timed groebner(pack)
+        println("Packing & 4ti2 & ", n, " & ", m, " & ", p, " & ", rep, " & ", size(gb, 2), " & ", size(gb, 1), " & ", t4ti2)
+        gb2, tipgbs, _, _, _ = @timed groebner_basis(pack)
+        println("Packing & IPGBs & ", n, " & ", m, " & ", p, " & ", rep, " & ", size(gb, 2), " & ", length(gb2), " & ", tipgbs)
+    end
+end
+
+using Random
+function compare_4ti2_ipgbs()
+    println("Algorithm & Problem & n & m & p & Rep & Vars & GBSize & Time")
+    Random.seed!(0)
+    for n in 5:5:20
+        test_lap(n, 30)
+    end
+    for n in 5:5:20
+        for m in 3:3:12
+            for p in 0.05:0.05:0.5
+                test_set_cover(n, m, p, 30)
+            end
+        end
+    end
+    for n in 5:5:20
+        for m in 3:3:12
+            for p in 0.05:0.05:0.5
+                test_set_packing(n, m, p, 30)
+            end
+        end
+    end
 end
