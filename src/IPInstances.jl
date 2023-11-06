f19a0ad9a3d468150f246075a655c07f3ad1296b
Gabriel Mattos Langeloh <gmlangeloh@inf.ufrgs.br>
2023-11-06 17:43:50 -0300

    Exporting apply_permutation

--- src/IPInstances.jl
+++ src/IPInstances.jl
@@ -1,6 +1,7 @@
 module IPInstances
 
-export IPInstance, nonnegative_vars, is_bounded, unboundedness_proof, update_objective!, nonnegativity_relaxation, group_relaxation, truncation_weight, projection, project_vector, unbounded_variables, is_feasible_solution, add_constraint, lattice_basis_projection
+export IPInstance, nonnegative_vars, is_bounded, unboundedness_proof, update_objective!, nonnegativity_relaxation, group_relaxation, truncation_weight, projection, project_vector, unbounded_variables, is_feasible_solution, add_constraint, lattice_basis_projection,
+apply_permutation
 
 import LinearAlgebra: I
 
